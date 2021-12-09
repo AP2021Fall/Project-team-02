a826d9ec9c7c08daec4467fd110ebe427eb33034
arya tamkhisha <aryabazy@gmail.com>
2021-12-09 18:54:43 +0330

    Menu

--- APProject/src/View/Menu.java
+++ APProject/src/View/Menu.java
@@ -1,16 +1,26 @@
 package View;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Menu {
+    protected Scanner scanner = new Scanner(System.in) ;
     private String name;
     protected Menu parent;
-    protected ArrayList<Menu> subMenus;
-    protected ArrayList<Menu> allMenus;
-    public Scanner scanner;
+    protected HashMap<Integer , Menu> subMenus ;
+    static protected ArrayList<Menu> allMenus;
+    static ArrayList<Menu> getAllMenus() {
+        return allMenus ;
+    }
+    public Menu(String name , Menu parent) {
+        setName(name) ;
+        setParent(parent);
+        allMenus.add(this) ;
+    }
 
-    public Menu() {
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getName() {
@@ -21,17 +31,40 @@ public class Menu {
         this.parent = parent;
     }
 
-    public void setSubMenus(ArrayList<Menu> subMenus) {
-        this.subMenus = subMenus;
+    public Menu getParent() {
+        return parent ;
     }
 
-    public void setScanner(Scanner scanner) {
-        this.scanner = scanner;
+   public HashMap<Integer,Menu> getSubMenus() {
+        return subMenus ;
+    }
+
+    protected String getInput() {
+        return scanner.nextLine() ;
+    }
+    protected String[] parseInput(String input) {
+        return input.split("\\s+");
     }
     public void show(){
-        System.out.println("salam     ");
+        System.out.println("--------" + this.name + "--------");
+        if (this.parent != null) {
+            System.out.println("Use Back to Previous Menu");
+        }
+        else {
+            System.out.println("Use Exit To Exit the Program");
+        }
     }
-    public void execute(){
-
+    public void execute() {
+        Menu nextMenu = this;
+        String input = getInput() ;
+        String[] inputParse = parseInput(input) ;
+        if (inputParse[0].equalsIgnoreCase("exit")) {
+            System.exit(1);
+        }
+        else if (inputParse[0].equalsIgnoreCase("back")) {
+            nextMenu = parent ;
+        }
+        nextMenu.show();
+        nextMenu.execute();
     }
 }
