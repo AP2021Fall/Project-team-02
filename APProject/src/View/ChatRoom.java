6df3d8c92f008a13e0b5e9d1bd08008e1c42a8df
arya tamkhisha <aryabazy@gmail.com>
2021-12-10 16:22:49 +0330

    Menu

--- APProject/src/View/ChatRoom.java
+++ APProject/src/View/ChatRoom.java
@@ -1,7 +1,33 @@
 package View;
 
 public class ChatRoom extends TeamMenu{
-    public void showChatroom() {
+    private String team ;
+    public ChatRoom(String name, Menu parent , String teamName) {
+        super(name, parent);
+        this.team = teamName ;
     }
 
+    public void show() {
+        super.show();
+        System.out.println("Team Chatroom :");
+        System.out.println(getChatRoomName(team));
+        System.out.println("send your message to chatroom:send message <message>");
+    }
+
+    public void execute() {
+        String input = getInput() ;
+        while(!input.equalsIgnoreCase("back")) {
+            String[] inputParse = parseInput(input) ;
+            if(inputParse[0].trim().equalsIgnoreCase("send")) {
+                sendMessage(inputParse[2]) ;
+            }
+            else {
+                System.out.println("wrong input!");
+            }
+            input = getInput() ;
+        }
+        this.nextMenu = parent ;
+        nextMenu.show();
+        nextMenu.execute();
+    }
 }
