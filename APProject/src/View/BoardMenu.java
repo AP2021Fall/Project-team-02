7baff61f96c7b9b423df4b8c660485f754322a3b
arya tamkhisha <aryabazy@gmail.com>
2021-12-10 13:35:29 +0330

    Menu

--- APProject/src/View/BoardMenu.java
+++ APProject/src/View/BoardMenu.java
@@ -1,5 +1,8 @@
 package View;
 
 public class BoardMenu extends TeamMenu{
+    public BoardMenu(String name, Menu parent) {
+        super(name, parent);
+    }
 
 }
--- APProject/src/View/CalendarMenu.java
+++ APProject/src/View/CalendarMenu.java
@@ -1,8 +1,28 @@
 package View;
 
 public class CalendarMenu extends Menu{
-    public void showDeadlines() {
-
+    public CalendarMenu(String name, Menu parent) {
+        super(name, parent);
     }
 
+    public void show() {
+        super.show();
+        System.out.println("Use this to see your calendar: calendar show deadline");
+    }
+
+    public void execute() {
+        String input = getInput() ;
+        String[] inputParse = parseInput(input) ;
+        if(inputParse[0].trim().equalsIgnoreCase("back")) {
+            this.nextMenu = parent ;
+            nextMenu.show();
+            nextMenu.execute();
+        }
+        if(inputParse[2].trim().equalsIgnoreCase("deadLines")) {
+            System.out.println(showDeadlines());
+            this.nextMenu = parent ;
+        }
+        nextMenu.show();
+        nextMenu.execute();
+    }
 }
