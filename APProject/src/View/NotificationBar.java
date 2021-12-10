ab4dbfc65b43e7c3b9e5b77d2cb29057afc654cc
arya tamkhisha <aryabazy@gmail.com>
2021-12-10 19:43:33 +0330

    Menu

--- APProject/src/View/NotificationBar.java
+++ APProject/src/View/NotificationBar.java
@@ -1,8 +1,21 @@
 package View;
 
 public class NotificationBar extends Menu{
-    public void showNotifications(){
-
+    public NotificationBar(String name, Menu parent) {
+        super(name, parent);
     }
 
+    public void show() {
+        super.show();
+        System.out.println(Allnutifs());
+    }
+
+    public void execute() {
+        String input = getInput() ;
+        if(input.equals("back")) {
+            this.nextMenu = parent ;
+            nextMenu.show();
+            nextMenu.execute();
+        }
+    }
 }
