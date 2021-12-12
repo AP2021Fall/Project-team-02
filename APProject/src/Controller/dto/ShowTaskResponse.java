dc36cdbcd4c8eaf27f2280d01f2baa0489ca2a7a
anita <AnitaAlikhani12@gmail.com>
2021-12-13 00:28:51 +0330

    ShowTaskResponse

--- APProject/src/Controller/dto/ShowTaskResponse.java
+++ APProject/src/Controller/dto/ShowTaskResponse.java
@@ -1,4 +1,33 @@
 package Controller.dto;
 
+import Model.Task;
+
 public class ShowTaskResponse {
+    private String message;
+    private Task task;
+
+    public ShowTaskResponse(String message) {
+        this.message = message;
+    }
+
+    public ShowTaskResponse(Task task) {
+        this.task = task;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
 }
+
