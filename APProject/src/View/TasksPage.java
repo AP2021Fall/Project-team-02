9c30360a17ad7f8efe2b54bdf3c1489eec2e6d2a
arya tamkhisha <aryabazy@gmail.com>
2021-12-12 19:26:01 +0330

    TaskPage!

--- APProject/src/View/TasksPage.java
+++ APProject/src/View/TasksPage.java
@@ -9,15 +9,16 @@ import Model.Task;
         public void show() {
             super.show();
             System.out.println(showTasks());
-            System.out.println("------------------------------------------");
-            System.out.println("Use this formats : ");
-            System.out.println("change title:edit task id <id> title <new title>");
-            System.out.println("change description:edit task id <id> description <new description>");
-            System.out.println("change priority:edit task id <id> priority <new priority>");
-            System.out.println("change deadLine:edit task id <id> deadLine <new deadLine>");
-            System.out.println("remove user(s) from task:edit task id <id> assignedUsers <username,username ..> remove");
-            System.out.println("add user(s) to task: edit task id <id> assignedUsers <username,username ..> add");
-
+            if(userLeader()) {
+                System.out.println("------------------------------------------");
+                System.out.println("Use this formats : ");
+                System.out.println("change title:edit task id <id> title <new title>");
+                System.out.println("change description:edit task id <id> description <new description>");
+                System.out.println("change priority:edit task id <id> priority <new priority>");
+                System.out.println("change deadLine:edit task id <id> deadLine <new deadLine>");
+                System.out.println("remove user(s) from task:edit task id <id> assignedUsers <username,username ..> remove");
+                System.out.println("add user(s) to task: edit task id <id> assignedUsers <username,username ..> add");
+            }
         }
         public void execute() {
             String input = getInput() ;
@@ -29,27 +30,27 @@ import Model.Task;
             }
             else if(Regex.changeTitle(input)) {
                 System.out.println(changeTitle1(inputParse[3] , inputParse[5]));
-                // if
+                this.nextMenu = this ;
             }
             else if(Regex.changeDescription(input)) {
                 System.out.println(changeDescription1(inputParse[3] , inputParse[5]));
-                // if
+                this.nextMenu = this ;
             }
             else if(Regex.changePriority(input)) {
                 System.out.println(changePriority1(inputParse[3] , inputParse[5]));
-                // if
+               this.nextMenu = this ;
             }
             else if(Regex.changeDeadline(input)) {
                 System.out.println(changeDeadline1(inputParse[3] , inputParse[5]));
-                // if
+                this.nextMenu = this ;
             }
             else if(Regex.removeUser(input)) {
                 System.out.println(removeUser1(inputParse[3] , inputParse[5]));
-                // if
+                this.nextMenu = this ;
             }
             else if(Regex.addUser(input)) {
                 System.out.println(addUser1(inputParse[3] , inputParse[5]));
-                // if
+                this.nextMenu = this ;
             }
             nextMenu.show();
             nextMenu.execute();
