fdd872ce00a6725b7544058ef672cd30db0f37b7
anita <AnitaAlikhani12@gmail.com>
2021-12-13 00:36:21 +0330

    LoginResponse

--- APProject/src/Controller/dto/LoginResponse.java
+++ APProject/src/Controller/dto/LoginResponse.java
@@ -1,4 +1,52 @@
 package Controller.dto;
 
 public class LoginResponse {
+    private Integer userId;
+    private String username;
+    private String role;
+    private String message;
+
+    public LoginResponse(Integer userId, String username, String role, String message) {
+        this.userId = userId;
+        this.username = username;
+        this.role = role;
+        this.message = message;
+    }
+
+    public LoginResponse(String message) {
+        this.message = message;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
