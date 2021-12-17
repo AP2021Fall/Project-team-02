856dd0b59eafcc2876f5c5b28af3322140c47fe0
anita <AnitaAlikhani12@gmail.com>
2021-12-17 20:46:28 +0330

    admin send message to members

--- APProject/src/Model/MessageType.java
+++ APProject/src/Model/MessageType.java
@@ -4,5 +4,6 @@ public class MessageType {
     public static final String TEAM = "team";
     public static final String TEAM_LEADER = "team_leader";
     public static final String USER = "user";
-    public static final String ALL = "all";
+    public static final String ADMIN = "admin";
+
 }
