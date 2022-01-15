08fcc38be4d0c4f0fe4267231c76c05819d44988
anita <AnitaAlikhani12@gmail.com>
2022-01-15 13:50:14 +0330

    ScoreBoard

--- APProject/src/View/ScoreBoard.java
+++ APProject/src/View/ScoreBoard.java
@@ -1,28 +1,33 @@
 package View;
 
 import Controller.Controller;
+import Controller.dto.ScoreBoardResponse;
 
 public class ScoreBoard extends TeamMenu {
-    String team ;
-    public ScoreBoard(String name, Menu parent , String team) {
-        super(name, parent);
+    private String team ;
+    private Controller controller = new Controller();
+    public ScoreBoard(String name, Menu parent, String team, String username, String role) {
+        super(name, parent, username, role);
         this.team = team ;
     }
 
     public void show() {
         super.show();
-        System.out.println(Controller.showScoreBoard(team));
+        ScoreBoardResponse scoreBoardResponse = controller.showScoreBoard(team);
+        if (scoreBoardResponse.isSuccessful()){
+            scoreBoardResponse.getUsersScores().forEach((username, score) -> System.out.println(username +" : " + score));
+
+        }else{
+            System.out.println(scoreBoardResponse.getMessage());
+        }
     }
 
     public void execute() {
         String input = getInput();
-        if(input.equals("back")) {
-            this.nextMenu = parent ;
-        }
-        else {
+        if (!input.equals("back")) {
             System.out.println("invalid input");
-            this.nextMenu = parent ;
         }
+        this.nextMenu = parent ;
         nextMenu.show();
         nextMenu.execute();
     }
