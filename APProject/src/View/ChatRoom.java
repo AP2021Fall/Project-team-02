a168ec81b18c4bff6e0505f6f57fdd4af5d50d16
anita <AnitaAlikhani12@gmail.com>
2022-01-15 13:45:10 +0330

    ChatRoom

--- APProject/src/View/ChatRoom.java
+++ APProject/src/View/ChatRoom.java
@@ -1,19 +1,32 @@
 package View;
 
 import Controller.Controller;
+import Controller.dto.ChatRoomResponse;
+
+import java.util.List;
 
 public class ChatRoom extends TeamMenu {
     private String team;
-
-    public ChatRoom(String name, Menu parent, String teamName) {
-        super(name, parent);
+    private Controller controller = new Controller();
+    public ChatRoom(String name, Menu parent, String teamName, String username, String role) {
+        super(name, parent, username, role);
         this.team = teamName;
     }
 
     public void show() {
         super.show();
         System.out.println("Team Chatroom :");
-        System.out.println(getChatRoomName(team));
+        ChatRoomResponse chatRoomResponse = controller.showChatRoom(team);
+        if (chatRoomResponse.isSuccessful()){
+            List<String> messages = chatRoomResponse.getMessages();
+            List<String> senders = chatRoomResponse.getSenders();
+
+            for (int i = 0; i < senders.size(); i++) {
+                System.out.println(senders.get(i) + " : " + messages.get(i) );
+            }
+        }else {
+            System.out.println(chatRoomResponse.getMessage());
+        }
         System.out.println("send your message to chatroom:send message <message>");
     }
 
@@ -22,12 +35,11 @@ public class ChatRoom extends TeamMenu {
         while (!input.equalsIgnoreCase("back")) {
             String[] inputParse = parseInput(input);
             if (Regex.chatRoom(input)) {
-              Controller.sendMessage(inputParse[2]) ;
-              this.nextMenu = this ;
-            }
-            else {
+                controller.sendMessage(getUsername(), team, inputParse[2]);
+                this.nextMenu = this;
+            } else {
                 System.out.println("invalid input!");
-                this.nextMenu = parent ;
+                this.nextMenu = parent;
             }
             nextMenu.show();
             nextMenu.execute();
